8b4f5dc4fcfd74347e093b12450dc6fa2672a98d
Icyfiremario <icyfiremario797@gmail.com>
2023-06-04 14:46:27 -0600

    inital commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import sqlite3
+from sqlite3 import Error
+import sys
+
+userSel = 0
+querySel = 0
+customQuery = ""
+
+curConnection = None
+database = None
+create_users_table = "CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, age INTERGER, gender TEXT, nationality TEXT);"
+create_users = "INSERT INTO users(name, age, gender, nationality) VALUES ('James', 15, 'male', 'USA'), ('Katelyn', 13, 'female', 'USA')"
+
+tables = None
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection success")
+    except Error as e:
+        print(f"Error: {e}")
+    
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"Error: {e}")
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"Error: {e}")
+
+while userSel != 4:
+    print("----Selection an Option----")
+    print("1. Connect to Database\n2. Execute query\n3. Show stored tables\n4. Exit\n")
+
+    userSel = int(input())
+    print("\n")
+
+    if userSel == 1:
+        database = input("Path to database:\n")
+        curConnection = create_connection(database)
+        tables = execute_read_query(curConnection, "SELECT name FROM sqlite_master WHERE type='table';")
+    
+    elif userSel == 2:
+        if(curConnection == None):
+            print("No Database loaded")
+        else:
+            print("----Preset Queries----")
+            print("1.Create users table\n2. Create users\n3. Custom query\n")
+            querySel = int(input())
+
+            if querySel == 1:
+                execute_query(curConnection, create_users_table)
+            elif querySel == 2:
+                execute_query(curConnection, create_users)
+            elif querySel == 3:
+                print("Input query below: \n")
+                customQuery = input()
+                execute_query(curConnection, customQuery)
+    elif userSel == 3:
+        if curConnection == None:
+            print("Not connected to database")
+        else:
+            print(tables)
+    else:
+        print("Not an option.")
+
+
+print("Exiting")
+sys.exit()
